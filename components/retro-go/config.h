903b807c9bdac66b6c7d0ab83d75c429a4fe9e11
Ashkan Shirian <ashkan.shirian@live.com>
2026-06-23 16:03:15 +0330

    added N16R8 target

--- components/retro-go/config.h
+++ components/retro-go/config.h
@@ -18,6 +18,8 @@
 #include "targets/esp32-s3-devkit/config.h"
 #elif defined(RG_TARGET_ESP32_S3_N8R2_ST7796)
 #include "targets/esp32-s3-n8r2-st7796/config.h"
+#elif defined(RG_TARGET_ESP32_S3_N16R8_ST7796)
+#include "targets/esp32-s3-n16r8-st7796/config.h"
 #elif defined(RG_TARGET_FRI3D_2024)
 #include "targets/fri3d-2024/config.h"
 #elif defined(RG_TARGET_BYTEBOI_REV1)
