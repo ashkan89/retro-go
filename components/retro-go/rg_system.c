c162465b4836dbaca50fd3d2eda1cf73064fda86
Ashkan Shirian <ashkan.shirian@live.com>
2026-07-25 02:14:28 +0330

    restore sd spi speed

--- components/retro-go/rg_system.c
+++ components/retro-go/rg_system.c
@@ -191,7 +191,7 @@ static rmt_encoder_handle_t led_rmt_encoder;
 static rmt_channel_t led_rmt_channel = RG_GPIO_LED_RMT_CHANNEL;
 #endif
 static bool led_rmt_initialized;
-static uint8_t brightness = 215;
+static uint8_t brightness = 200;
 
 static inline uint8_t scale(uint8_t v, uint8_t brightness)
 {
